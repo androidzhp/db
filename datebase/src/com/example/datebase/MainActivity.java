c74aba45f8ffe9f7baebeacdab2ab3fc99d519f4
kabir <jxwazxzhp@126.com>
2016-07-21 01:01:51 +0800

    use android api

--- datebase/src/com/example/datebase/MainActivity.java
+++ datebase/src/com/example/datebase/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.datebase;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -23,21 +24,34 @@ public class MainActivity extends Activity {
 	// add
 	public void click1(View v) {
 		SQLiteDatabase db = mMyOpenHelper.getWritableDatabase();
-		db.execSQL("insert into info(name, phone) values(?, ?);", new Object[] { "James", "13881384138" });
+		ContentValues values = new ContentValues();
+		values.put("name", "王五");
+		values.put("phone", "120");
+		long insert = db.insert("info", null, values);
 		db.close();
+		if (insert > 0) {
+			Toast.makeText(this, "添加成功", Toast.LENGTH_SHORT).show();
+		} else {
+			Toast.makeText(this, "添加失败", Toast.LENGTH_SHORT).show();
+
+		}
 	}
 
 	// delete
 	public void click2(View v) {
 		SQLiteDatabase db = mMyOpenHelper.getWritableDatabase();
-		db.execSQL("delete from info where name=?", new Object[] { "James" });
+		int delete = db.delete("info", "name=?", new String[] { "王五" });
+		Toast.makeText(this, "删除了" + delete + "行", Toast.LENGTH_SHORT).show();
 		db.close();
 	}
 
 	// update
 	public void click3(View v) {
 		SQLiteDatabase db = mMyOpenHelper.getWritableDatabase();
-		db.execSQL("update info  set phone=? where name=?", new Object[] { "13888888888", "James" });
+		ContentValues values = new ContentValues();
+		values.put("phone", "189");
+		int update = db.update("info", values, "name=?", new String[] { "王五" });
+		Toast.makeText(this, "更新了" + update + "行", Toast.LENGTH_SHORT).show();
 		db.close();
 
 	}
@@ -45,13 +59,11 @@ public class MainActivity extends Activity {
 	// find
 	public void click4(View v) {
 		SQLiteDatabase db = mMyOpenHelper.getReadableDatabase();
-		Cursor cursor = db.rawQuery("select * from info", null);
+		Cursor cursor = db.query("info", new String[] { "phone" }, "name=?", new String[] { "王五" }, null, null, null);
 		if (cursor != null && cursor.getCount() > 0) {
 			while (cursor.moveToNext()) {
-				String name = cursor.getString(1);
-				String phone = cursor.getString(2);
-				String buf = String.format("name: %s phone: %s", name, phone);
-				Toast.makeText(this, buf, Toast.LENGTH_SHORT).show();
+				String phone = cursor.getString(0);
+				Toast.makeText(this, "phone is " + phone, Toast.LENGTH_SHORT).show();
 			}
 		}
 	}
